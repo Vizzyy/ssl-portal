2bd7d304c488ad578b4d5f2d49dd13089aff0eb2
Vizzyy <barnabas.vizy@gmail.com>
2019-01-13 16:13:09 -0500

    Pull out logging from portal

--- constants.js
+++ constants.js
@@ -4,19 +4,29 @@ const sslPath = '/home/pi/login/ssl/',
     owner = ["vizzyy-ddns-net-barney"],
     ca = [
         fs.readFileSync(sslPath + 'vizzyy-ddns-net-barney.crt'),
-        fs.readFileSync(sslPath + 'vizzyy_ddns_net.crt'),
+        fs.readFileSync(sslPath + 'vizzyy_ddns_net.ca-bundle'),
         fs.readFileSync(sslPath + 'dinkleToPortal.crt'),
         fs.readFileSync(sslPath + 'portal.crt')
-    ];
-
-module.exports = {
-    sslOptions: {
+    ],
+    mainSSL = {
         ca: ca,
         key: fs.readFileSync(sslPath + 'portal.key'),
         cert: fs.readFileSync(sslPath + 'portal.crt'),
         requestCert: true,
         rejectUnauthorized: true
     },
+    portalToDinkle = {
+        uri: 'https://vizzyy.ddns.net/log/append?entry=', json: true, host: 'vizzyy.ddns.net',
+        port: 443, path: '/', method: 'GET',
+        key: mainSSL.key,
+        cert: mainSSL.cert,
+        ca: ca,
+        rejectUnauthorized: true
+    };
+
+module.exports = {
+    sslOptions: mainSSL,
+    portalToDinkle: portalToDinkle,
     port: 443,
     admins: admins,
     owner: owner
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const exec = require('child_process').exec;
 const statusPath = '/home/pi/login/logs/status.txt';
-const logPath = '/home/pi/login/logs/portalLog.txt';
+const fs = require('fs'),
+	rp = require('request-promise');
+const constants = require('../constants');
 
 router.get('/', function(req, res) {
 	res.render('index', {
@@ -17,49 +19,53 @@ function execute(command, res) {
 			console.error('error:', error);
 			return;
 		}
-		//console.log(stdout);
 		if(res)
-			res.send(stdout.split('\n').reverse());
-		else 
-			return;
+			res.send(stdout.replace("\n", ""));
+		else
+			return stdout;
 	});
 }
 
 router.get('/open', function(req, res) {
-	let logEntry = req.query.entry;
-	execute("/usr/bin/python /home/pi/scripts/unlock.py");
-	execute("echo '" + logEntry + "' >> " + logPath); //TODO: This is extremely bad
-	execute("echo 'Opened' > " + statusPath);
-	console.log(logEntry);
+	let logEntry = req.query.entry + '\n';
+	var requestOptions = Object.assign({}, constants.portalToDinkle);
+	requestOptions.uri = requestOptions.uri+logEntry;
+
+	execute("/usr/bin/python /home/pi/scripts/unlock.py"); // Physically move lock
+
+	rp(requestOptions).then(function(body) {	// Make call to logging
+		console.log(logEntry);
+	}).catch(err => {
+		console.log(err);
+	});
+
+	execute("echo 'Opened' > " + statusPath); // set Local state for lock
+
 	res.end();
 });
 
 router.get('/close', function(req, res) {
-	let logEntry = req.query.entry;
-	execute("/usr/bin/python /home/pi/scripts/lock.py");
-	execute("echo '" + logEntry + "' >> " + logPath);
-	execute("echo 'Closed' > " + statusPath);
-	console.log(logEntry);
-	res.end();
-});
+	let logEntry = req.query.entry + '\n';
+	var requestOptions = Object.assign({}, constants.portalToDinkle);
+	requestOptions.uri = requestOptions.uri+logEntry;
 
-router.get('/log', function(req, res) {
-	let cmd = "cat " + logPath;
-	console.log(cmd);
-	execute(cmd, res);
+	execute("/usr/bin/python /home/pi/scripts/lock.py");
+
+	rp(requestOptions).then(function(body) {
+		console.log(logEntry);
+	}).catch(err => {
+		console.log(err);
+	});
+
+	execute("echo 'Closed' > " + statusPath);
+
+	res.end();
 });
 
 router.get('/status', function(req, res) {
 	let cmd = "cat " + statusPath;
 	console.log(cmd);
-
-	exec(cmd, function(error, stdout, stderr) {
-		if (error) {
-			console.error('error:', error);
-			return;
-		}
-		res.send(stdout.split('\n')[0]);	// This is also bad
-	});
+	execute(cmd, res);
 });
 
 module.exports = router;
