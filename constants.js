1eec2aa0585d168680a1c5c851ea28d1b64a7ed9
Barnabas <Vizzyy@users.noreply.github.com>
2019-01-12 21:10:04 -0500

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,32 +1,22 @@
-const sslPath = '/home/pi/login/ssl/',
-	fs = require('fs');
-
-const ca = [
-	//fs.readFileSync(sslPath + 'vizzyy_ddns_net.ca-bundle'),
-	fs.readFileSync(sslPath+'vizzyy-ddns-net-barney.crt'),
-    fs.readFileSync(sslPath+'dinkleToLights.crt'),
-    fs.readFileSync(sslPath+'lights.crt')
-];
-//
-// const dinkleToPortal = {
-//     key: fs.readFileSync('/home/barney/client509/clientprivate.key'),
-//     cert: fs.readFileSync('/home/barney/client509/dinkleToPortalCert.crt'),
-//     ca: fs.readFileSync(sslPath+'portal.crt')
-// }
-//
-const admins = ["vizzyy-ddns-net-barney", "dinkleToLights"];
-const owner = ["vizzyy-ddns-net-barney"];
+const 	sslPath = '/home/pi/login/ssl/',
+	fs = require('fs'),
+      	admins = ["vizzyy-ddns-net-barney", "dinkleToLights"],
+      	owner = ["vizzyy-ddns-net-barney"],
+	ca = [
+		fs.readFileSync(sslPath+'vizzyy-ddns-net-barney.crt'),
+    		fs.readFileSync(sslPath+'dinkleToPortal.crt'),
+    		fs.readFileSync(sslPath+'portal.crt')
+	];
 
 module.exports = {
 	sslOptions : {
 		ca : ca,
-		key : fs.readFileSync(sslPath + 'lights.key'),
-		cert : fs.readFileSync(sslPath + 'lights.crt'),
-        requestCert: true,
-        rejectUnauthorized: true
+		key : fs.readFileSync(sslPath + 'portal.key'),
+		cert : fs.readFileSync(sslPath + 'portal.crt'),
+        	requestCert: true,
+        	rejectUnauthorized: true
 	},
-	port: 443,  //3000
-    admins: admins,
+	port: 443, 
+    	admins: admins,
 	owner: owner
-    // dinkleToPortal: dinkleToPortal
 };
